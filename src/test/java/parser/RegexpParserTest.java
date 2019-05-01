c9ff410b08aeea9375e70fe017d81ab8149994f5
ritt <margarita.fedotova@rtlabs.ru>
2019-05-01 11:14:23 +0000

    test runs

--- src/test/java/parser/RegexpParserTest.java
+++ src/test/java/parser/RegexpParserTest.java
@@ -1,44 +1,90 @@
 package parser;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 import org.junit.Before;
 import org.junit.Test;
-import regexp.Choice;
-import regexp.Factor;
-import regexp.RegExp;
-import regexp.Sequence;
+import regexp.*;
 
+import static enums.Quantifier.NONE;
+import static enums.Quantifier.STAR;
 import static org.junit.Assert.*;
 
 public class RegexpParserTest {
 
-    Sequence one;
+    private RegExp expectedEasy;
+    private RegExp expectedHard;
 
     @Before
     public void setUp() throws Exception {
-//        one = new Sequence();
+        expectedEasy = new Sequence(
+                Factor.builder()
+                        .base(Choice.builder()
+                                .one(Factor.builder()
+                                        .base(new Base('a'))
+                                        .quantifier(NONE)
+                                        .build())
+                                .two(Factor.builder()
+                                        .base(new Base('b'))
+                                        .quantifier(NONE)
+                                        .build())
+                                .build())
+                        .quantifier(STAR)
+                        .build(),
+                Factor.builder()
+                        .base(new Base('d'))
+                        .quantifier(NONE)
+                        .build());
 
-//        one.addRegEx(new Choice(new Factor()));
+        expectedHard = Factor.builder()
+                .base(Choice.builder()
+                        .one(Factor.builder()
+                                .base(new Base('0'))
+                                .quantifier(NONE)
+                                .build())
+                        .two(Factor.builder()
+                                .base(new Sequence((
+                                        Factor.builder()
+                                                .base(new Base('1'))
+                                                .quantifier(NONE)
+                                                .build()),
+                                        Factor.builder()
+                                                .base(new Sequence(
+                                                        Factor.builder()
+                                                                .base(new Base('0'))
+                                                                .quantifier(NONE)
+                                                                .build(),
+                                                        Factor.builder()
+                                                                .base(new Base('1'))
+                                                                .quantifier(STAR)
+                                                                .build(),
+                                                        Factor.builder()
+                                                                .base(new Base('0'))
+                                                                .quantifier(NONE)
+                                                                .build()
+                                                ))
+                                                .quantifier(STAR)
+                                                .build(),
+                                        Factor.builder()
+                                                .base(new Base('1'))
+                                                .quantifier(NONE)
+                                                .build()))
+                                .quantifier(NONE)
+                                .build())
+                        .build())
+                .quantifier(STAR)
+                .build();
     }
 
     @Test
-    public void regex() {
-        RegexpParser regexpParser1 = new RegexpParser("(a|b)*d", 0);
-        RegexpParser regexpParser2 = new RegexpParser("(0|(1(01*0)*1))*", 0);
-
-
-        RegExp regex1 = regexpParser1.regex();
+    public void easyRegex() {
+        RegexpParser regexpParser = new RegexpParser("(a|b)*d", 0);
+        RegExp actual = regexpParser.regex();
+        assertEquals(actual, expectedEasy);
     }
 
     @Test
-    public void term() {
-    }
-
-    @Test
-    public void factor() {
-    }
-
-    @Test
-    public void base() {
+    public void hardRegex() {
+        RegexpParser regexpParser = new RegexpParser("(0|(1(01*0)*1))*", 0);
+        RegExp actual = regexpParser.regex();
+        assertEquals(actual, expectedHard);
     }
 }
