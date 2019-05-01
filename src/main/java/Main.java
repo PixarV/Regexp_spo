622ba44e1554c510474937f43439bcebf74412e9
ritt <margarita.fedotova@rtlabs.ru>
2019-05-01 10:07:10 +0000

    more examples in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,14 @@
-import lombok.val;
 import parser.RegexpParser;
-import regexp.RegEx;
+import regexp.RegExp;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey");
         RegexpParser regexpParser = new RegexpParser("(0|(1(01*0)*1))*", 0);
-        RegEx regex = regexpParser.regex();
+        RegExp regex = regexpParser.regex();
+        System.out.println("hoi");
+        RegexpParser regexpParser1 = new RegexpParser("(a|b)*d", 0);
+        RegExp regex1 = regexpParser1.regex();
         System.out.println("hoi");
     }
 }
